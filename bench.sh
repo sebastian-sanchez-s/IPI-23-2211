384f7ed4b4e74368dfd98b0e5e5cffac47889a6c
Sebastián Sánchez <slsanchez@uc.cl>
2023-07-28 07:37:36 -0400

    Move .sh scripts to root

---
+++ bench.sh
@@ -0,0 +1,29 @@
+SAMPLES=5
+ROOTDIR=$(pwd)/perf/
+
+mkdir -p $ROOTDIR
+
+echo "Benchmark for StandardYoungTableux Computation."
+echo "Samples per case: "$SAMPLES
+
+CASES="3x5 4x5"
+
+for c in $CASES
+do
+    echo -n "Case $c" 
+
+    m=$(echo $c | cut -d"x" -f1)
+    n=$(echo $c | cut -d"x" -f2)
+
+    perf_file=$ROOTDIR"m"$m"n"$n
+    if [[ ! -f "$perf_file" ]]; then
+        mkdir -p raw
+        3>$perf_file perf stat --null -r $SAMPLES --log-fd 3 obj/a.out $m $n
+        echo $(tail -2 $perf_file | head) > $perf_file
+    fi
+
+    echo " [DONE]" 
+    cat $perf_file
+
+    rm -f raw/*
+done
